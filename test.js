21fb8527aa159aa9251dd3de666d24590a0b44d3
shrubfromtomorrow <orionjandrews@gmail.com>
2022-10-09 12:36:46 -0700

    Added timer functionality and got turn switching to work. Added switch feature to change who plays black and who plays white. Also added a stop button. Things to work on are: getting zeros in front of minutes or seconds with a length less than 1 and adding pause functionality

--- test.js
+++ test.js
@@ -1,3 +1,8 @@
+defaultTime = [01, 10];
+document.querySelector('#topTimerText').innerHTML = defaultTime[0]+':'+defaultTime[1];
+document.querySelector('#bottomTimerText').innerHTML = defaultTime[0]+':'+defaultTime[1];
+
+
 let topDiv = document.querySelector("#top");
 let topActive = false;
 let audioTop = document.getElementById("audioTop");
@@ -8,13 +13,18 @@ function codeTop () {
         audioTop.pause();
         // audioTop.currentTime = 0;
         topActive = false;
+        topDiv.innerHTML = "";
     }
     else {
         if (bottomActive) {
             bottomActive = false;
             audioBottom.pause();
+            clearInterval(bottomTimerInterval);
             // audioBottom.currentTime = 0;
         }
+        topTimer();
+        bottomDiv.innerHTML = "";
+        topDiv.innerHTML = "<span class='icon-clock'></span>";
         topActive = true;
         audioTop.play();
     }
@@ -28,16 +38,93 @@ bottomDiv.onclick = codeBottom;
 function codeBottom () {
     if (bottomActive) {
         audioBottom.pause();
-        // audioBottom.currentTime = 0;
+        audioBottom.currentTime = 0;
         bottomActive = false;
+        bottomDiv.innerHTML = "";
     }
     else {
         if (topActive) {
             topActive = false;
             audioTop.pause();
+            clearInterval(topTimerInterval);
             // audioTop.currentTime = 0;
         }
+        bottomTimer();
+        topDiv.innerHTML = "";
+        bottomDiv.innerHTML = "<span class='icon-clock'></span>";
         bottomActive = true;
         audioBottom.play();
+    }
+}
+
+let stopButton = document.querySelector("#stop");
+let stopActive = true;;
+stopButton.onclick = stopcode;
+function stopcode () {
+    window.location.reload();
+}
+
+let topTimerInterval;
+let currentTopTimeMin = defaultTime[0];
+let currentTopTimeSec = defaultTime[1];
+
+function topTimer(){
+    var min = currentTopTimeMin;
+    var sec = currentTopTimeSec;
+    // document.querySelector('#topTimerText').innerHTML=min+':'+sec;
+    topTimerInterval = setInterval(function(){
+        sec--;
+        currentTopTimeMin = min;
+        currentTopTimeSec = sec;
+        if (sec < 00 && min <= 00) {
+            clearInterval(topTimerInterval);
+        }
+        else if (sec < 00 && min > 00) {
+            sec = sec + 60;
+            min--;
+        }
+        document.querySelector('#topTimerText').innerHTML=min+':'+sec;
+    }, 1000);
+}
+
+let bottomTimerInterval;
+let currentBottomTimeMin = defaultTime[0];
+let currentBottomTimeSec = defaultTime[1];
+
+function bottomTimer(){
+    var min = currentBottomTimeMin;
+    var sec = currentBottomTimeSec;
+    // document.querySelector('#topTimerText').innerHTML=min+':'+sec;
+    bottomTimerInterval = setInterval(function(){
+        sec--;
+        currentBottomTimeMin = min;
+        currentBottomTimeSec = sec;
+        if (sec < 00 && min <= 00) {
+            clearInterval(bottomTimerInterval);
+        }
+        else if (sec < 00 && min > 00) {
+            sec = sec + 60;
+            min--;
+        }
+        document.querySelector('#bottomTimerText').innerHTML=min+':'+sec;
+    }, 1000);
+}
+
+let switchButton = document.querySelector("#switch");
+let switches = 0;
+switchButton.onclick = function() {
+    if (switches % 2 == 0) {
+        topDiv.style.backgroundColor = "#24273a";
+        bottomDiv.style.backgroundColor = "#5b6078";
+        topDiv.style.color = "#5b6078";
+        bottomDiv.style.color = "#24273a";
+        switches++;
+    }
+    else {
+        topDiv.style.backgroundColor = "#5b6078";
+        bottomDiv.style.backgroundColor = "#24273a";
+        topDiv.style.color = "#24273a";
+        bottomDiv.style.color = "#5b6078";
+        switches++;
     }
 }
